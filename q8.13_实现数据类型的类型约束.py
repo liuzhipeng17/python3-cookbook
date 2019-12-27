e79f6dae0fe42b3289fd2cafb2cea86b0054269f
liuzhipeng17 <liucpliu@sina.cn>
2019-12-27 10:01:36 +0800

    Update q8.13_实现数据类型的类型约束.py

--- q8.13_实现数据类型的类型约束.py
+++ q8.13_实现数据类型的类型约束.py
@@ -1,5 +1,7 @@
 
-# 你想定义某些属性赋值上有限制的数据结构
+#问题： 你想定义某些属性赋值上有限制的数据结构
+
+#解决方案： 1使用描述器实现一个系统类型和赋值验证框架
 
 class Descriptor:
     def __init__(self,name=None, **opts):
@@ -40,6 +42,39 @@ class Typed(Descriptor):
               raise ValueError('size must  be < ' + str(self.size))
           super().__set__(instance, value)
       
+# 解决方案：2根据上面的基础构建模块，定义各种不同的数据类型
+
+class Integer(Typed):
+    expected_type = int
+
+class UnsignedInteger(Integer, Unsigned):
+    pass
+
+class Float(Typed):
+    expected_type = float
+   
+class UnsignedFloat(Float, Unsigned):
+    pass
+
+class String(Typed):
+    expected_type = str
+    
+class SizedString(String, Maxsized):
+    pass
+
+    
+# 解决方案：3根据自定义类型，定义一个类
+class Stock:
+    name = SizedString('name', size=8)
+    shares = UnsignedInteger('share')
+    price = UnsignedFloat('price')
+    
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+    
+
           
           
           
